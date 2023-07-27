a895f9d7bbca3534eb20e4ff5b3b1a85746f388c
Jeffrey Yu <92849+wehjin@users.noreply.github.com>
2023-07-27 13:14:30 -0700

    Hardcodes toggle state into  the websocket.

--- src/main.rs
+++ src/main.rs
@@ -40,50 +40,14 @@ async fn main() -> std::io::Result<()> {
 
 use actix::{Actor, StreamHandler};
 use serde_json::Value;
+use crate::protocol::PhxMsgType;
 
-struct MyWs;
+struct MyWs {
+	using_emoji: bool,
+}
 
 impl Actor for MyWs {
 	type Context = ws::WebsocketContext<Self>;
-}
-
-fn to_reply(msg: &Value) -> Option<String> {
-	match msg {
-		Value::Array(vec) => {
-			let reply_payload = match vec[3].as_str().unwrap() {
-				"phx_join" => {
-					Some(json!({
-						"status":"ok",
-						"response":{
-							"rendered":{
-								"0":"👋",
-								"1":"Liveviewjstest",
-								"2":"Use Text",
-								"s":["\n      <div class=\"flex flex-col items-center space-y-10 pt-10\">\n        <div class=\"flex flex-col items-center space-y-5\">\n          <h1 class=\"text-2xl font-bold\">"," ","</h1>\n          <button class=\"bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded\" phx-click=\"toggle\">\n            ","\n          </button>\n        </div>\n        <div class=\"text-center max-w-[200px]\">\n          More documentation and examples at\n          <a class=\"text-blue-500\" href=\"https://liveviewjs.com\" target=\"_blank\" rel=\"noopener noreferrer\"\n            >LiveViewJS.com</a\n          >\n        </div>\n      </div>\n    "]
-							}
-						}
-					}))
-				}
-				"heartbeat" => {
-					Some(json!({
-						"status":"ok",
-						"response":{}
-					}))
-				}
-				&_ => None
-			};
-			let reply = reply_payload.map(|payload| json!([
-				vec[0].clone(),
-				vec[1].clone(),
-				vec[2].clone(),
-				"phx_reply",
-				payload
-			]));
-			let reply = reply.map(|it| it.to_string());
-			reply
-		}
-		_ => None
-	}
 }
 
 impl StreamHandler<Result<ws::Message, ws::ProtocolError>> for MyWs {
@@ -95,7 +59,7 @@ impl StreamHandler<Result<ws::Message, ws::ProtocolError>> for MyWs {
 			}
 			Ok(ws::Message::Text(text)) => {
 				let json: Value = serde_json::from_str(&text.to_string()).unwrap();
-				let reply = to_reply(&json);
+				let reply = self.to_reply(&json);
 				println!("{:?}", reply);
 				if let Some(reply) = reply {
 					ctx.text(reply);
@@ -113,10 +77,83 @@ impl StreamHandler<Result<ws::Message, ws::ProtocolError>> for MyWs {
 	}
 }
 
+impl MyWs {
+	fn to_reply(&mut self, msg: &Value) -> Option<String> {
+		match msg {
+			Value::Array(vec) => {
+				let msg_type = PhxMsgType::from(&vec[3]);
+				let response = match msg_type {
+					PhxMsgType::Join => {
+						let json = json!({
+							"rendered":{
+								"0":"👋",
+								"1":"Liveviewjstest",
+								"2":"Use Text",
+								"s":["\n      <div class=\"flex flex-col items-center space-y-10 pt-10\">\n        <div class=\"flex flex-col items-center space-y-5\">\n          <h1 class=\"text-2xl font-bold\">"," ","</h1>\n          <button class=\"bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded\" phx-click=\"toggle\">\n            ","\n          </button>\n        </div>\n        <div class=\"text-center max-w-[200px]\">\n          More documentation and examples at\n          <a class=\"text-blue-500\" href=\"https://liveviewjs.com\" target=\"_blank\" rel=\"noopener noreferrer\"\n            >LiveViewJS.com</a\n          >\n        </div>\n      </div>\n    "]
+							}
+						});
+						Some(json)
+					}
+					PhxMsgType::Heartbeat => {
+						let json = json!({});
+						Some(json)
+					}
+					PhxMsgType::Event => {
+						let json = if self.using_emoji {
+							json!({"diff":{"0":"Hello","2":"Use Emoji"}})
+						} else {
+							json!({"diff":{"0":"👋","2":"Use Text"}})
+						};
+						self.using_emoji = !self.using_emoji;
+						Some(json)
+					}
+					PhxMsgType::Unknown(_) => None,
+				};
+				let reply = response.map(|response| json!([
+				vec[0].clone(),
+				vec[1].clone(),
+				vec[2].clone(),
+				"phx_reply",
+				{
+					"status":"ok",
+					"response":response
+				}
+			]));
+				let reply = reply.map(|it| it.to_string());
+				reply
+			}
+			_ => None
+		}
+	}
+}
+
 async fn ws_index(req: HttpRequest, stream: web::Payload) -> Result<HttpResponse, actix_web::Error> {
-	let resp = ws::start(MyWs {}, &req, stream);
+	let resp = ws::start(MyWs { using_emoji: true }, &req, stream);
 	println!("{:?}", resp);
 	resp
 }
 
-mod protocol {}
+pub mod protocol {
+	use serde_json::Value;
+	use crate::protocol::PhxMsgType::{Event, Heartbeat, Join};
+
+	#[derive(Debug, Clone)]
+	pub enum PhxMsgType {
+		Join,
+		Heartbeat,
+		Event,
+		Unknown(String),
+	}
+
+	impl From<&Value> for PhxMsgType {
+		fn from(value: &Value) -> Self {
+			let s = value.as_str().unwrap();
+			match s {
+				"phx_join" => Join,
+				"heartbeat" => Heartbeat,
+				"event" => Event,
+				&_ => PhxMsgType::Unknown(s.to_string())
+			}
+		}
+	}
+}
